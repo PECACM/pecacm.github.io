dfbb8852fe7d5bdf051dd4f61e2f11d3f317a988
TheNsBhasin <nsbhasincool@gmail.com>
2020-05-08 01:47:26 +0530

    Deploy develop branch

---
+++ precache-manifest.243d8dc5a57955f1b5306df684d65057.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d597b18cb7cd8e4731ce54636dde92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bdad6a4373379a9e832",
+    "url": "/static/css/main.fb8b2657.chunk.css"
+  },
+  {
+    "revision": "7d3bf088ca5b92fc7c86",
+    "url": "/static/js/2.d187d7db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d187d7db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67869c32c1b043fa979",
+    "url": "/static/js/3.98206a78.chunk.js"
+  },
+  {
+    "revision": "6ce76b0ab81cab2472bb",
+    "url": "/static/js/4.9573919b.chunk.js"
+  },
+  {
+    "revision": "feacb4b3e44e40c7d082",
+    "url": "/static/js/5.aa4681dd.chunk.js"
+  },
+  {
+    "revision": "1218b210ed5d2113d629",
+    "url": "/static/js/6.622efee2.chunk.js"
+  },
+  {
+    "revision": "2bdad6a4373379a9e832",
+    "url": "/static/js/main.d6fb9532.chunk.js"
+  },
+  {
+    "revision": "a0460acf3ab3a4475e35",
+    "url": "/static/js/runtime-main.261a4d5b.js"
+  }
+]);
